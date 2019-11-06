124c4899c1e226fdc65762885a963edd21a681df
guygoldgit <guygoldfeld26@gmail.com>
2019-11-06 21:31:56 +0200

    asd

---
+++ R/Rexercise2.R
@@ -0,0 +1,9 @@
+Rank1 <- c(1,2,3,4,5)
+Peak <- c(1,2,3,4,5)
+Title1 <- c("Avatar","Titanic","Star Wars","Avengers","Jurrasic World")
+Worldwide_gross  <- c(2787,2187,2068,1844,1671)
+Year <- c(2009,1997,2015,2018,2015)
+df12<- data.frame(Rank1,Peak,Title1,Worldwide_gross,Year)
+
+df12
+df12$Title1[3]
