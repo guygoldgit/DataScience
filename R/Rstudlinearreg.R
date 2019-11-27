e50242f09093b43d5803188b0bfcf09da9af35c3
guygoldgit <guygoldfeld26@gmail.com>
2019-11-27 21:29:36 +0200

    hihihih

---
+++ R/Rstudlinearreg.R
@@ -0,0 +1,30 @@
+data <- Orange
+head(data)
+mod <- lm(age~circumference,data=data)
+summary(mod)
+yhat <- predict(mod,data)
+plot(yhat ~data$age)
+scatter.smooth(yhat~data$age)
+
+
+
+
+data<-iris
+head(data)
+data$y <- ifelse(data$Species=="versicolor",1,0)
+data$Species<- NULL
+rnd <- sample(seq(1,nrow(data)))
+data <- data[rnd]
+mod <- glm(factor(y) ~ Sepal.Width + Petal.Length ,family="binomial",data = data)
+summary(mod)
+pred <- predict(mod,data,type="response")
+hist(pred)
+yhat <- ifelse(pred>=0.5,1,0)
+table(yhat=yhat,y=data$y)
+library(car)
+vif(mod)
+accuracy <- ((88+21)/nrow(data))
+accuracy
+
+
+vif(mod)
